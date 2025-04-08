bcdf4811b06a428c54b6fb9f033b564aa98e935a
lichangjiang <changjiang.li.up@outlook.com>
2025-04-08 15:31:06 +0500

    optimize: ChoiceModal

--- app/(tabs)/(people)/index.tsx
+++ app/(tabs)/(people)/index.tsx
@@ -1,24 +1,26 @@
-import { FlatList, StyleSheet } from 'react-native';
+import {FlatList, StyleSheet} from 'react-native';
 
-import { VFull } from "@/components/VFull";
-import { router } from "expo-router";
-import { Styles } from "@/constants/Styles";
-import { Text, ToastPresets } from "react-native-ui-lib";
-import { descSortStorage } from "@/store/storage";
-import { useContext } from "react";
-import { ToastContext } from "@/components/provider/ToastProvider";
-import { newPerson, Person, stateChoicesPeople, statePeople, statePerson } from "@/store/state";
-import { PlatformPressable } from "@react-navigation/elements";
+import {VFull} from "@/components/VFull";
+import {router} from "expo-router";
+import {Styles} from "@/constants/Styles";
+import {Text, ToastPresets} from "react-native-ui-lib";
+import {descSortStorage} from "@/store/storage";
+import {useContext, useMemo} from "react";
+import {ToastContext} from "@/components/provider/ToastProvider";
+import {newPerson, Person, stateChoicesPeople, statePeople, statePerson} from "@/store/state";
+import {PlatformPressable} from "@react-navigation/elements";
 import LargeBtn from "@/components/ui/LargeBtn";
 
 export default function TabPeopleScreen() {
-    const people = Object.values(statePeople(state => state.record)) as Person[];
-    const { showToast } = useContext(ToastContext);
+    const peopleRecord = statePeople(state => state.record);
+    const people = useMemo(() => Object.values(peopleRecord), [peopleRecord]);
+
+    const {showToast} = useContext(ToastContext);
     const choices = stateChoicesPeople(state => state.record);
 
     const styles = getStyles();
 
-    function renderItem({ item }: { item: Person }) {
+    function renderItem({item}: { item: Person }) {
         return <PlatformPressable
             onPress={upsertPerson(item)}
             key={item.key}
@@ -31,16 +33,16 @@ export default function TabPeopleScreen() {
                 onPress={() => {
                     statePeople.getState().deleteRecord(item.key);
                     showToast('Person deleted', ToastPresets.OFFLINE);
-                }} />
+                }}/>
         </PlatformPressable>;
     }
 
     return (
         <VFull>
             <FlatList data={descSortStorage(people)}
-                style={[Styles.flexG1, Styles.ph5]}
-                keyExtractor={({ key }) => key}
-                renderItem={renderItem}>
+                      style={[Styles.flexG1, Styles.ph5]}
+                      keyExtractor={({key}) => key}
+                      renderItem={renderItem}>
             </FlatList>
             <LargeBtn
                 label='Add Peron'
@@ -59,6 +61,6 @@ function upsertPerson(person?: Person) {
 
 function getStyles() {
     return StyleSheet.create({
-        itemText: { flexGrow: 1, fontWeight: '500' },
+        itemText: {flexGrow: 1, fontWeight: '500'},
     });
 }
